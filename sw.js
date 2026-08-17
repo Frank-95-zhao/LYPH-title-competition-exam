ee30b73700d40521925885df4e7105af6a7114d1
Frank-95-zhao <73415515+Frank-95-zhao@users.noreply.github.com>
2026-08-17 22:05:39 +0800

    Expand bank to atomic knowledge-point coverage

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='lyph-title-competition-v3-20260817';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./data/questions.json','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='lyph-title-competition-v4-atomic-20260817';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./data/questions.json','./data/questions/physician.json','./data/questions/nurse.json','./data/questions/infectious.json','./data/questions/hand_hygiene.json','./data/questions/core_systems.json','./data/questions/discipline.json','./data/questions/integrity.json','./data/questions/patient_relations.json','./data/questions/fire_safety.json','./data/questions/medical_waste.json','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response}).catch(()=>event.request.mode==='navigate'?caches.match('./index.html'):undefined)))});
